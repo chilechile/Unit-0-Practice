b9f69449ba34d0d3edf51f4aaaf525af936c4b88
chilechile <amontgomery313@noscihigh.org>
2021-03-08 18:18:03 +0000

    i completed the github

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 Write a greeter function that will display 
 'Hello World' to the console.
 */
-
+function myFuncGreet = "Hello World"
 
 
 
@@ -20,8 +20,9 @@ divide(3, 9) => 12
 divide(9, 9) => 18
 
 */
-
-
+console.log(2 / 4); {
+  return the sum of two numbers
+}
 
 
 /* 3.
@@ -33,7 +34,9 @@ divide(3, 9) => -6
 divide(9, 9) => 0
 
 */
-
+console.log(3 < >9); {
+  return the differnce of two numbers
+}
 
 
 
@@ -46,7 +49,9 @@ divide(3, 9) => 0.3333333
 divide(9, 9) => 1
 
 */
-
+console.log(2, 4): {
+  return the quotient of two numbers
+}
 
 
 
@@ -60,7 +65,9 @@ multiply(3, 9) => 27
 multiply(9, 9) => 81
 
 */
-
+console.log(2 * 4); {
+  return the product of two numbers
+}
 
 
 
@@ -74,7 +81,9 @@ yearsToDays(3) => 1095
 yearsToDays(9) => 3285
 
 */
-
+function myFuncYearsToDays = 1;{
+  return years into days
+}
 
 
 
@@ -88,6 +97,9 @@ firstElement([0,1,2,3]) => 0
 yearsToDays(['hello', 'world']) => 'hello'
 
 */
+function myFunctionFirstElement= ([0, 1, 2, 3]); {
+  return the first element given array 
+}
 
 
 
@@ -113,15 +125,19 @@ yearsToDays(['hello', 'world']) => 'hello'
 /* 8.
 Write a function that will convert minutes into 
 seconds. 
-
+function myFuncMinToSec = 30 {
+  convert mins to sec
+}
 
 Example:
 minToSec(60) => 3600
 minToSec(1) => 60
 
 */
-
-
+function myFuncMinToSec = 30 {
+ return minutes to seconds 
+  console.log
+}
 
 
 
@@ -137,7 +153,10 @@ secToHours(3600) => 1
 secToHours(1800) => 0.5
 
 */
-
+function myFuncSecToHour = 3600; {
+  return sec to hour 
+  console.log 
+}
 
 
 
@@ -154,3 +173,5 @@ remainder(5, 2) => 1
 remainder(10, 7) => 0.5
 
 */
+console.log(5, 2);
+return remainder of two numbers 
